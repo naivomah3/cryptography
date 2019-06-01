07a496badf35efb3c406dc1b4341456a3f3826e0
Mahery Ranaivoson <njato@outlook.fr>
2019-06-02 03:04:01 +0530

    args parsing

--- MD4/md4.py
+++ MD4/md4.py
@@ -1,26 +1,20 @@
-import sys
 import argparse
+from codecs import decode
+from os import sys
 from modules import helper as h
 
-def main():
 
-    '''
-    parser = argparse.ArgumentParser(description="Voting Client")
+def main():
+    # Args parsing 
+    parser = argparse.ArgumentParser(description="Hashing algorithm")
     parser.add_argument('-m', '--message', action='store', dest='message', help='The message to hash', required=True)
     args = parser.parse_args()
-    '''
 
-    if len(sys.argv) != 2:
-        print("Usage: \npython3 md4.py \"<your_message>\"")
-        print("# Note: \"<your_message>\" should be within double quotes\n")
-        exit()
-
-    message = sys.argv[1]
+    message = args.message
     print("\nOriginal message ({1} bits) :\n  \"{0}\" ".format(message, len(message)*8))
-    
-    md4_hash = h.md4(message, True)
-    print("\nHash (128 bits) : {}\n".format(md4_hash.upper()))
+
+    md4_hash = h.md4(message, False)
+    print("\nHash value (128 bits) : {}\n".format(md4_hash.decode('utf-8')))
 
 if __name__ == "__main__":
     main()
-    
